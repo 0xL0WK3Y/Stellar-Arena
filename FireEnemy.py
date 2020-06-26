6af9606020612f132747f2a00676371ab27be953
George Rellias <lokarrcursed@protonmail.com>
2020-06-26 21:19:35 +0300

    Texture code and Chase Player function
    The enemy object now has proper texture code and a function that allows it to chase the player around the map.

--- FireEnemy.py
+++ FireEnemy.py
@@ -1,4 +1,11 @@
-import arcade
+import arcade, math, random
+
+def load_texture_pair(filename):
+
+    return[
+        arcade.load_texture(filename),
+        arcade.load_texture(filename, mirrored=True)
+    ]
 
 class FireEnemyObject(arcade.Sprite):
     
@@ -21,3 +28,49 @@ class FireEnemyObject(arcade.Sprite):
         self.updates_per_frame = 7
 
         self.fire_sprite = arcade.Sprite("C:\\Users\\Dell\\Desktop\\Pylam\\Project\\Sprites\\player.png")
+        self.idle_sprite = load_texture_pair("C:\\Users\\Dell\\Desktop\\Pylam\\Arcade\\Sprites\\player.png")
+
+        self.walk_textures = []
+        for i in range(1,2,1):
+            texture = load_texture_pair("C:\\Users\\Dell\\Desktop\\Pylam\\Arcade\\Sprites\\player_move_"+str(i)+".png")
+            self.walk_textures.append(texture)
+
+    def update_animation(self, delta_time: float=1/60):
+        
+        if self.change_x < 0 and self.character_face_direction == self.face_right:
+            self.character_face_direction = self.face_left
+        elif self.change_x > 0 and self.character_face_direction == self.face_left:
+            self.character_face_direction = self.face_right
+            
+        if self.change_x == 0 and self.change_y == 0:
+            self.texture = self.idle_sprite[0]
+            return
+        if self.current_texture > 2 * self.updates_per_frame:
+            self.current_texture = 0
+        self.texture = self.walk_textures[self.current_texture // self.updates_per_frame][self.character_face_direction]
+
+
+    def chase_player(self, player):
+
+        self.player = player
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if random.randrange(100) == 0:
+
+            start_x = self.center_x
+            start_y = self.center_y
+
+            dest_x = self.player.center_x
+            dest_y = self.player.center_y
+
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            self.change_x = math.cos(angle) * self.mv_speed
+            self.change_y = math.sin(angle) * self.mv_speed
+            
+
+
